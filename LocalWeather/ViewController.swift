41bcaa1c54140a038f406e7323acccbb0ee06376
Sibin <sibinbk@gmail.com>
2015-11-16 11:34:30 +1100

    Initial Commit

---
+++ LocalWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalWeather
+//
+//  Created by Sibin Baby on 16/11/2015.
+//  Copyright © 2015 Sibin Baby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
