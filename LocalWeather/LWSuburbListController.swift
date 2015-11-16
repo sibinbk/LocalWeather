cc14c9ef57c4b2b0d7c1a56a7b7b5d63ef4e4d42
Sibin <sibinbk@gmail.com>
2015-11-17 04:30:59 +1100

    Parsed JSON data stores into CoreData

--- LocalWeather/LWSuburbListController.swift
+++ LocalWeather/LWSuburbListController.swift
@@ -7,40 +7,209 @@
 //
 
 import UIKit
+import CoreData
 
-class LWSuburbListController: UITableViewController {
+class LWSuburbListController: UITableViewController, NSFetchedResultsControllerDelegate {
+  
+  let urlString = "https://dnu5embx6omws.cloudfront.net/venues/weather.json"
+  let ReuseIdentifierCell = "SuburbCell"
+  
+  var managedObjectContext: NSManagedObjectContext!
+  
+  lazy var fetchedResultsController: NSFetchedResultsController = {
+    // Initialize Fetch Request.
+    let fetchRequest = NSFetchRequest(entityName: "Venue")
+    
+    // Add Sort Descriptors
+    let sortDescriptor = NSSortDescriptor(key: "venueName", ascending: true)
+    fetchRequest.sortDescriptors = [sortDescriptor]
+    
+    // Initialize Fetched Results Controller.
+    let fetchedResultsController = NSFetchedResultsController (fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+    
+    // Configure Fetched Results Controller
+    fetchedResultsController.delegate = self
+    
+    return fetchedResultsController
+  }()
+  
+  // MARK: - View Life Cycle
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = false
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    self.getWeatherData()
   }
-    
-  // MARK: - Table view data source
   
+  @IBAction func reloadWeatherData(sender: UIBarButtonItem) {
+    print("Refresh button pressed")
+  }
+  
+  func getWeatherData() {
+    let appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    self.managedObjectContext = appDelegate.managedObjectContext
+    
+    let url = NSURL(string: self.urlString)
+    let session = NSURLSession.sharedSession()
+    
+    let dataTask = session.dataTaskWithURL(url!, completionHandler: { (data,response, error) -> Void in
+      
+      do {
+        let jsonResult = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+        
+        guard let weatherData = jsonResult["data"] as? NSArray else {
+          print("No data to load")
+          return
+        }
+        
+        print(weatherData.count)
+        
+        // Deleting existing Core Data entries before reloading json data.
+        let request = NSFetchRequest(entityName: "Venue")
+        
+        do {
+          let oldData = try self.managedObjectContext.executeFetchRequest(request)
+          
+          if oldData.count > 0 {
+            for item in oldData {
+              self.managedObjectContext.deleteObject(item as! NSManagedObject)
+            }
+            
+            do {
+              try self.managedObjectContext?.save()
+            } catch {
+              let saveError = error as NSError
+              print("\(saveError), \(saveError.userInfo)")
+            }
+          }
+        } catch {
+          print("error")
+        }
+        
+        for dataItem in weatherData {
+          let newItem: NSManagedObject = NSEntityDescription.insertNewObjectForEntityForName("Venue", inManagedObjectContext: self.managedObjectContext)
+          
+          if let venueName = dataItem["_name"] as? String {
+            newItem.setValue(venueName, forKey: "venueName")
+          }
+          
+          if let country = dataItem["_country"] as? NSDictionary {
+            if let countryName = country["_name"] as? String {
+              newItem.setValue(countryName, forKey: "country")
+            }
+          }
+        }
+        
+        do {
+          try self.managedObjectContext?.save()
+        } catch {
+          let saveError = error as NSError
+          print("\(saveError), \(saveError.userInfo)")
+          
+          self.showAlertWithTitle("Warning", message: "Your to-do could not be saved.", cancelButtonTitle: "OK")
+        }
+        
+        do {
+          try self.fetchedResultsController.performFetch()
+        } catch {
+          let fetchError = error as NSError
+          print("\(fetchError), \(fetchError.userInfo)")
+        }
+        
+        dispatch_async(dispatch_get_main_queue()) {
+          self.tableView.reloadData()
+        }
+        
+      }
+      catch {
+        print("Error: \(error)")
+      }
+      
+    })
+    
+    dataTask.resume()
+  }
+  
+  // MARK:- Helper Methods
+  private func showAlertWithTitle(title: String, message: String, cancelButtonTitle: String) {
+    // Initialize Alert Controller
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+    
+    // Configure Alert Controller
+    alertController.addAction(UIAlertAction(title: cancelButtonTitle, style: .Default, handler: nil))
+    
+    // Present Alert Controller
+    presentViewController(alertController, animated: true, completion: nil)
+  }
+  
+  // MARK: - Table view data source
   override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    // #warning Incomplete implementation, return the number of sections
+    if let sections = fetchedResultsController.sections {
+      return sections.count
+    }
+    
     return 0
   }
   
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    // #warning Incomplete implementation, return the number of rows
+    if let sections = fetchedResultsController.sections {
+      let sectionInfo = sections[section]
+      return sectionInfo.numberOfObjects
+    }
+    
     return 0
   }
   
-  /*
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-  let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-  
-  // Configure the cell...
-  
-  return cell
+    let cell = tableView.dequeueReusableCellWithIdentifier(ReuseIdentifierCell, forIndexPath: indexPath)
+    
+    let venueInfo = fetchedResultsController.objectAtIndexPath(indexPath)
+    
+    if let venueName = venueInfo.valueForKey("venueName") as? String {
+      cell.textLabel?.text = venueName
+    }
+    
+    if let country = venueInfo.valueForKey("country") as? String {
+      cell.detailTextLabel?.text = country
+    }
+    
+    return cell
   }
-  */
+  
+  // MARK:- Fetched Results Controller Delegate Methods
+  func controllerWillChangeContent(controller: NSFetchedResultsController) {
+    tableView.beginUpdates()
+  }
+  
+  func controllerDidChangeContent(controller: NSFetchedResultsController) {
+    tableView.endUpdates()
+  }
+  
+  func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+    switch (type) {
+    case .Insert:
+      if let indexPath = newIndexPath {
+        tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+      }
+      break;
+    case .Delete:
+      if let indexPath = indexPath {
+        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+      }
+      break;
+    case .Update:
+      break;
+    case .Move:
+      if let indexPath = indexPath {
+        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+      }
+      
+      if let newIndexPath = newIndexPath {
+        tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Fade)
+      }
+      break;
+    }
+  }
+  
   
   /*
   // Override to support conditional editing of the table view.
